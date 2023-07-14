5d74ea1d32b368ba86cea60477e725e11e4e7c94
Camilo <camilocraft08@gmail.com>
2023-07-14 12:18:38 -0400

    fixed a bug on environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private weatherService: WeatherService){
+   
+  };
+
+  ngOnInit(): void {
+      this.weatherService.getWeather('New York').subscribe({
+        next: (response) => {
+          console.log(response);
+        }
+      });
+  }
   title = 'Weather-App';
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,32 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Weather } from '../models/Weather.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  url: string;
-  hostHeaderName: string;
-  hostHeaderValue: string;
-  hostKeyName: string;
-  hostKeyValue: string;
-
-
   constructor(protected http: HttpClient) {
-    this.url = environment.apiUrl;
-    this.hostHeaderName = environment.xRapidAPIHostHeaderName;
-    this.hostHeaderValue = environment.xRapidAPIHostHeaderValue;
-    this.hostKeyName = environment.xRapidAPIKeyHerderName;
-    this.hostKeyValue = environment.xRapidAPIKeyHerderValue;
-  }
 
-  getWeather(cityName: string){
-    return this.http.get(this.url, {
+  };
+
+  getWeather(cityName: string): Observable<Weather> {
+    return this.http.get<Weather>(environment.apiUrl, {
       headers: new HttpHeaders()
-        .set(this.hostHeaderName, this.hostHeaderValue)
-        .set(this.hostKeyName, this.hostKeyValue),
+        .set(environment.xRapidAPIHostHeaderName, environment.xRapidAPIHostHeaderValue)
+        .set(environment.xRapidAPIKeyHerderName, environment.xRapidAPIKeyHerderValue),
       params: new HttpParams()
         .set('city', cityName)
     })
