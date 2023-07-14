9f164efa0c3bff118d16f7c517afa1ef4d5c326c
Camilo <camilocraft08@gmail.com>
2023-07-14 11:40:52 -0400

    Created http get in weather.service. Created weather.model

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  url: string;
+  hostHeaderName: string;
+  hostHeaderValue: string;
+  hostKeyName: string;
+  hostKeyValue: string;
+
+
+  constructor(protected http: HttpClient) {
+    this.url = environment.apiUrl;
+    this.hostHeaderName = environment.xRapidAPIHostHeaderName;
+    this.hostHeaderValue = environment.xRapidAPIHostHeaderValue;
+    this.hostKeyName = environment.xRapidAPIKeyHerderName;
+    this.hostKeyValue = environment.xRapidAPIKeyHerderValue;
+  }
+
+  getWeather(cityName: string){
+    return this.http.get(this.url, {
+      headers: new HttpHeaders()
+        .set(this.hostHeaderName, this.hostHeaderValue)
+        .set(this.hostKeyName, this.hostKeyValue),
+      params: new HttpParams()
+        .set('city', cityName)
+    })
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,8 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    apiUrl: 'https://weather-api99.p.rapidapi.com/weather',
+    xRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    xRapidAPIHostHeaderValue: 'weather-api99.p.rapidapi.com',
+    xRapidAPIKeyHerderName: 'X-RapidAPI-Key',
+    xRapidAPIKeyHerderValue: 'weather-api99.p.rapidapi.com'
+};
